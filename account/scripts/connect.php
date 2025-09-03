1787c2e8dc41cf0769c447e23956a7f5dd224101
wona-creater <johnchiza16@gmail.com>
2025-09-03 16:49:15 +0100

    done

--- account/scripts/connect.php
+++ account/scripts/connect.php
@@ -1,15 +1,15 @@
 <?php
 // for live server
-// $servername = "localhost";
-// $username = "iwebbtec_db24";
-// $password = "Vision@2020";
-// $dbname = "iwebbtec_db24";
-
-// for test
 $servername = "localhost";
 $username = "u379044712_omo";
 $password = "&e0$p94:S";
 $dbname = "u379044712_omo";
+
+// for test
+// $servername = "localhost";
+// $username = "u379044712_omo";
+// $password = "&e0$p94:S";
+// $dbname = "u379044712_omo";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
